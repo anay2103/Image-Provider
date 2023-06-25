803601f489e6cb5a5af03dcf09aeabe8b76f804f
Jana_Karau <yana-karausheva@yandex.ru>
2023-06-25 23:38:48 +0300

    minor corrections in read_csv

--- app/events.py
+++ app/events.py
@@ -23,16 +23,16 @@ async def read_csv(app: FastAPI) -> None:
     5 5 None 5 None
     1 None 1 None 1
 
-    Ряды отсортированы по убыванию кол-ва показов.
-
-    Маппинг рядов в ссылки на картинки сохраняется в словаре url_nums.
-    Маппинг колонок в категории сохраняется в словаре category_nums.
+    В кэше сохраняются ключи:
+        - NUMS - массив с кол-вом показов картинки;
+        - CATEGORIES - маппинг названия категорий в колонки массива NUMS;
+        - URLS - список ссылок на картинки, сортированный в порядке убывания показов.
     """
     categories_set = set()
     url_dict = {}
     with open('images.csv') as file:
         links = csv.reader(file)
-        for link in reversed(sorted([link for link in links], key=lambda x: int(x[1]))):
+        for link in reversed(sorted(links, key=lambda x: int(x[1]))):
             url, shows, *categories = link
             url_dict[url] = {'shows': int(shows), 'categories': categories}
             for cat in categories:
